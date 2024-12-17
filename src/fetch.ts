f192669dc8d7be1a60fe267c38cc8fb9337f8a5a
sy-jung-ulift <sy.jung@ulift.inc>
2024-12-17 16:19:45 +0900

    Fix: fetch 함수 개선사항 주석 표시

--- src/fetch.ts
+++ src/fetch.ts
@@ -5,9 +5,14 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const LAT = 37.65590833; // 지역의 위도를 넣어주세요!
 const LON = 126.7770556; // 지역의 경도를 넣어주세요!
 
+// const client = axios.create({
+//   baseURL: BASE_URL,
+// });
+
 export const weatherApi = {
   getWeatherByCity: async (): Promise<WeatherData> => {
     try {
+      //const response = await client.get<WeatherData>(
       const response = await axios.get<WeatherData>(
         `${BASE_URL}/weather?lat=${LAT}&lon=${LON}&appid=${process.env.REACT_APP_API_KEY}`
       );
